c4153d9568aafb4151a3baa0d6c4496bedcfcab5
0xeeLi <hi@0xee.li>
2025-03-18 22:57:45 +0100

    Preparation for packages recipe

---
+++ debug_recipe.sh
@@ -0,0 +1,36 @@
+#
+# Debug package: variables, functions and commands in a package recipe
+# Test: sli install debug (debug_recipe must be in the current dir)
+#
+
+debug_install() {
+	
+	PKG_NAME="debug"
+	PKG_VERSION="0.0.0"
+	SRC_URL="https://github.com/0xeeli/sli/releases/download"
+	TARBALL="${PKG_NAME}-${PKG_VERSION}.tar.gz"
+	SRC_DIR="${PKG_NAME}-${PKG_VERSION}"
+	DL_URL=${SRC_URL}/${PKG_VERSION}/${TARBALL}
+	
+	# Pre install
+	pkg_pre_install "$PKG_NAME" "$PKG_VERSION"
+	
+	# Check package recipe variables
+	echo -e "${CYAN}Package Variable${NC}"
+	
+	echo "PKG_NAME=${PKG_NAME}"
+	echo "PKG_VERSION=${PKG_VERSION}"
+	echo "SRC_URL=${SRC_URL}"
+	echo "TARBALL=${TARBALL}"
+	echo "SRC_DIR=${SRC_DIR}"
+	echo "DL_URL=${DL_URL}"
+	
+	# Check paths
+	echo -e "\n${CYAN}SLi paths${NC}"
+	
+	echo "SLI_DIR=${SLI_DIR}"
+	echo "INSTALLED_PKGS=${INSTALLED_PKGS}"
+	echo "CACHE_DIR=${SLI_DIR}/cache"
+	echo "PKGS_LIST=${SLI_DIR}/packages.list"
+}
+
